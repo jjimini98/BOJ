94d379ba174e9f23c86e0f803de5131fe37bd35d
jimin <jjimini98@naver.com>
2021-03-13 21:43:36 +0900

    add code practice.py

--- 입출력/0313/practice.py
+++ 입출력/0313/practice.py
@@ -1,10 +1,10 @@
-import sys
-
-number = map(int, sys.stdin.readline())
-print(number)
-
-for x in range(1,number):
-    print("*" * x)
+# import sys
+#
+# number = map(int, sys.stdin.readline())
+# print(number)
+#
+# for x in range(1,number):
+#     print("*" * x)
 
 
 #Traceback (most recent call last):
@@ -12,3 +12,7 @@ for x in range(1,number):
 #     for x in range(1,number):
 # TypeError: 'map' object cannot be interpreted as an integer
 # <map object at 0x0000029C3D70D4E0>
+
+x= 1
+soo = 2*(x-1)
+print(soo)
