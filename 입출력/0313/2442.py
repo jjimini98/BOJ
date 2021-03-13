1e408d9986781ad1acfcf74fbe5e5b19f6ba1b20
jimin <jjimini98@naver.com>
2021-03-13 21:45:11 +0900

    finished 2442.py

--- 입출력/0313/2442.py
+++ 입출력/0313/2442.py
@@ -1,12 +1,12 @@
-number = int(input())
-
-print(("*" ).center(number))
-for x in range(1,number):
-    print("{:^1}".format(("*" * (2*x +1))))
+# number = int(input())
+#
+# print(("*" ).center(number))
+# for x in range(1,number):
+#     print("{:^1}".format(("*" * (2*x +1))))
 
 
 
 # 정답
-# n = int(input())
-# for i in range(1,n+1):
-#     print(" "*(n-i) + "*" * (2*i-1))
+n = int(input())
+for i in range(1,n+1):
+    print(" "*(n-i) + "*" * (2*i-1))
