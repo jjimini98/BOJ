cd6f297f34950fabcc548198ac9f9e73cfdc422d
jimin <jjimini98@naver.com>
2021-03-17 12:17:30 +0900

    proceeding 10992.py

---
+++ 입출력/0314/10992.py
@@ -0,0 +1,16 @@
+# 별 찍기 - 17
+
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(1,n+1):
+
+    if i >=3 :
+        # print(" " * (n - 1) + "*" * (2 * 1 - 1))
+        print(" " * (n - i)  + "*" + " " * i + "*")
+        print(" " * (n-(i+1)) + "*" * (2 * i - 1))
+        break
+
+    else:
+        print(" " * (n - i) + "*" * (2 * i - 1))
