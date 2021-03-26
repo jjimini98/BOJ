33f0d3eeec7fc2a0dd88b867e5cf829a4bb31b76
jimin <jjimini98@naver.com>
2021-03-26 16:00:26 +0900

    modified 11652.py

--- Sorting/0323/11652.py
+++ Sorting/0323/11652.py
@@ -2,17 +2,19 @@
 import sys
 
 num = int(sys.stdin.readline())
-li = [0 for _ in range(num)]
+li = [0 for _ in range(num)]  #num짜리 리스트 하나 생성
 
 for x in range(num):
     num = int(sys.stdin.readline())
     if num < 0  :
         num1=abs(num)
         li[num1]+=1
+        max_num = li.index(max(li))
     else:
         li[num] += 1
 max_num = li.index(max(li))
-
+print("max num은", max_num)
+print("li는", li)
 if num<0:
     print(-max_num)
 else:
