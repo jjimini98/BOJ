71758af90b1d5b4890928ef30e00530084593aae
jimin <jjimini98@naver.com>
2021-03-27 18:32:22 +0900

    reviewed answer code

--- Sorting/0326/11652_answer.py
+++ Sorting/0326/11652_answer.py
@@ -4,12 +4,14 @@ import sys
 n = int(sys.stdin.readline())
 dic = {}
 
-for case in range(n):
+for _ in range(n):
     tmp = int(sys.stdin.readline())
     if tmp in dic:
         dic[tmp] += 1
     else:
         dic[tmp] = 1
 
+
 dic = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
 print(dic[0][0])
+# print("this is sorted dic : " ,dic)
