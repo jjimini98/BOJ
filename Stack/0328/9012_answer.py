75dd2d67d36a00a55ac8dc078936a990017867cf
jimin <jjimini98@naver.com>
2021-03-28 10:25:01 +0900

    created 9012_answer.py

---
+++ Stack/0328/9012_answer.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    stack = []
+    vps = input()
+    check = 0
+    for c in vps:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if len(stack) == 0:
+                print("NO")
+                check = 1
+                break
+
+            else:
+                stack.pop(-1)
+
+    if len(stack) != 0 and check == 0: print("NO")
+    elif len(stack) == 0 and check == 0: print("YES")
